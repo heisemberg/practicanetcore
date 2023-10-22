ab7dfea233595fa14e9ef98fc6a9cddcc5f74887
heisemberg <heissen1122@gmail.com>
2023-10-22 16:09:49 -0500

    conex base de datos

--- Infrastructure/Data/ApiContext.cs
+++ Infrastructure/Data/ApiContext.cs
@@ -1,12 +1,21 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data
 {
-    public class ApiContext
+    public class ApiContext : DbContext
     {
-        
+        public ApiContext (DbContextOptions options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                base.OnModelCreating(modelBuilder);
+                modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            }
     }
 }
